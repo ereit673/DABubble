6cb0168d566ff413dd5e23215d500c7108e61f48
Marcel Zalec <marci.zalec@hotmail.com>
2024-12-05 09:55:04 +0100

    add routes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import { BuilderComponent } from '../shared/builder/builder.component';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [HeaderComponent, BuilderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
