9cc4be94ed1b29f1bd882fbaff9665f25ac10026
Christoph Völker <christov@gmx.de>
2024-12-06 16:39:42 +0100

    animation

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -9,16 +9,42 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss',
   animations: [
-    trigger('openClose', [
-      state('closed', style({ "color": "white" })),
-      state('open', style({ "color": "green" })),
-      transition('closed <=> open', [animate('1s')])
+    trigger('startStop', [
+      state('start', style({})),
+      state('stop', style({ "top": "10%", "left": "10%", "width": "50px" })),
+      transition('start => stop', [animate('1s')])
     ]),
   ],
 })
 export class IntroComponent {
-  losBoolean = true;
-  protected menuState: 'open' | 'closed' = 'closed';
+  protected startAnim: 'start' | 'stop' = 'start';
 }
 
 bootstrapApplication(IntroComponent, { providers: [provideAnimationsAsync()] });
+
+
+// .icon {
+//   position: absolute;
+//   width: 100px;
+//   height: 121px;
+//   top: 50%;
+//   left: 50%;
+//   z-index: 2;
+//   transform: translate(-50%, -50%);
+//   animation: moveImage 1s forwards;
+//   animation-delay: 0.5s;
+// }
+
+// @keyframes moveImage {
+//   from {
+//     transform: translate(-50%, -50%);
+//     width: 100px;
+//     height: 121px;
+//   }
+//   to {
+//     left: 20%;
+//     top: 12%;
+//     width: 64px;
+//     height: 78px;
+//   }
+// }
