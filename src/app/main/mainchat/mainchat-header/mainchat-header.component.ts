8eeb988a6bcfd0e9532c7397450ba3e641174833
Lukas Nolting <info@lukas-nolting.eu>
2024-12-07 15:36:24 +0100

    added components & stylings

---
+++ src/app/main/mainchat/mainchat-header/mainchat-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainchat-header',
+  imports: [],
+  templateUrl: './mainchat-header.component.html',
+  styleUrl: './mainchat-header.component.scss'
+})
+export class MainchatHeaderComponent {
+
+}
---
+++ src/app/main/threadchat/threadchat-header/threadchat-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-threadchat-header',
+  imports: [],
+  templateUrl: './threadchat-header.component.html',
+  styleUrl: './threadchat-header.component.scss'
+})
+export class ThreadchatHeaderComponent {
+
+}
