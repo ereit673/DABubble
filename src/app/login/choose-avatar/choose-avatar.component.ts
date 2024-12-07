bf01e276545ca59c9f4c05de9489a9edb7eda392
Marcel Zalec <marci.zalec@hotmail.com>
2024-12-07 16:58:46 +0100

    added for loop for profile pics

--- src/app/login/choose-avatar/choose-avatar.component.ts
+++ src/app/login/choose-avatar/choose-avatar.component.ts
@@ -1,15 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoginComponent } from '../login.component';
 
 @Component({
   selector: 'app-choose-avatar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './choose-avatar.component.html',
   styleUrl: './choose-avatar.component.scss'
 })
 export class ChooseAvatarComponent {
   name:string = "Frederik Beck";
+  activePic:number = -1 ;
+  profilesPics: string[] = ['pic.svg','00c.Charaters (1).svg','00c.Charaters (2).svg','00c.Charaters (3).svg','00c.Charaters (4).svg','00c.Charaters (5).svg',];
+  path:string = "assets/svgs/";
 
   constructor(private router: Router) {}
   
@@ -19,5 +22,9 @@ export class ChooseAvatarComponent {
 
   toBoard() {
     this.router.navigateByUrl('board');
+  }
+
+  setActive(index:number) {
+    this.activePic = index;
   }
 }
