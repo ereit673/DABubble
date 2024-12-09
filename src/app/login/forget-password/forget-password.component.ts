8d93c89305196a99aec2879da380ba69823ade18
MarcelZalec <marci.zalec@hotmail.com>
2024-12-09 17:36:58 +0100

    disabled button in forget-password

--- src/app/login/forget-password/forget-password.component.ts
+++ src/app/login/forget-password/forget-password.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forget-password',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.scss'
 })
 export class ForgetPasswordComponent {
+  
+  form = {
+    email: "",
+  }
 
   constructor(private router: Router) {}
   
   back() {
     this.router.navigateByUrl('');
+  }
+
+  sendMail(ngForm: NgForm) {
+
   }
 }
