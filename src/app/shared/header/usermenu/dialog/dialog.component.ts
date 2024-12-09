c5ef2ca934722e0fd723ab0b88fd1c5ba2039c0f
Lukas Nolting <info@lukas-nolting.eu>
2024-12-09 20:52:31 +0100

    header dialog

--- src/app/shared/header/usermenu/dialog/dialog.component.ts
+++ src/app/shared/header/usermenu/dialog/dialog.component.ts
@@ -1,32 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dialog',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss',
 })
 export class DialogComponent {
+  @Input() dialog: boolean = false;
+  @Output() dialogChange = new EventEmitter<boolean>();
   profileDialog: boolean = false;
   profileDialogEdit: boolean = false;
 
- dontCloseDialog(event: Event) {
-  event?.preventDefault();
-  event.stopPropagation();
-
-}
-
-  logout(){
-    console.log("logout");
+  dontCloseDialog(event: Event) {
+    event?.preventDefault();
+    event.stopPropagation();
   }
 
-  openProfile(){
-    console.log("profile");
+  closeDialog(event: Event) {
+    event?.preventDefault();
+    event.stopPropagation();
+    this.dialog = false;
+    this.dialogChange.emit(this.dialog);
   }
 
-  openProfileEdit(){
-    console.log("profile edit");
+  logout() {
+    console.log('logout');
   }
-  
 
+  openProfile() {
+    this.profileDialog = true;
+    this.dialog = false;
+  }
+
+  openProfileEdit() {
+    this.profileDialog = false;
+    this.profileDialogEdit = true;
+  }
+
+  saveProfile() {
+    this.profileDialog = false;
+    this.profileDialogEdit = false;
+    this.dialogChange.emit(this.dialog);
+  }
 }
