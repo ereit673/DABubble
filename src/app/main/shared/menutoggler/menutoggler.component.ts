d332bc3141a55032bc0b8904bd169a5f23fac356
CybXII <luft.luft86@googlemail.com>
2024-12-11 22:05:23 +0100

    readjusted toggle menu button position

--- src/app/main/shared/menutoggler/menutoggler.component.ts
+++ src/app/main/shared/menutoggler/menutoggler.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input  } from '@angular/core';
 
 @Component({
   selector: 'app-menutoggler',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './menutoggler.component.html',
   styleUrl: './menutoggler.component.scss'
 })
 export class MenutogglerComponent {
-
+  @Input() menuState!: string;
 }
