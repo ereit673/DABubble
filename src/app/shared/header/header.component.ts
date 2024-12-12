f98dce1c405dd7c3c40c7cf316eaef807abc98f8
Christoph Völker <christov@gmx.de>
2024-12-12 19:38:36 +0100

    imprint/legal w/o header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
 import { UsermenuComponent } from './usermenu/usermenu.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,5 +11,13 @@ import { UsermenuComponent } from './usermenu/usermenu.component';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  currentUrl: string = '';
+
+  constructor(private router: Router) {
+    this.currentUrl = this.router.url;
+    console.log(this.currentUrl);
+
+  }
 
 }
