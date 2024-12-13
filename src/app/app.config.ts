89b3afc90c1afb70aa162a5598f073f044095985
Christoph Völker <christov@gmx.de>
2024-12-13 22:28:04 +0100

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-  provideAnimations(), provideAnimationsAsync(), provideAnimationsAsync()
+  provideAnimations(), provideAnimationsAsync(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"dabubble-4c4f8","appId":"1:784459986538:web:9b8067bc32c25b475dfc13","storageBucket":"dabubble-4c4f8.firebasestorage.app","apiKey":"AIzaSyChOdAlV4710eiJ2P6v9I-0YI0wlzhTSQ0","authDomain":"dabubble-4c4f8.firebaseapp.com","messagingSenderId":"784459986538"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())
   ]
 };
 
