3372cc6d4b8d7baf660482dd51c099dac7558ed6
CybXII <luft.luft86@googlemail.com>
2024-12-22 15:19:46 +0100

    -styling bugfix menu img position -renamed private messages to privateChannels

--- src/app/main/menu/menu-private-messages/menu-private-messages.component.ts
+++ src/app/main/menu/menu-private-messages/menu-private-messages.component.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
 export class MenuPrivateMessagesComponent {
   messagesOpen: boolean = false
 
-  privateMessages: { id: number; name: string ; imgPath: string ; status: boolean }[]=[  //Example Data
+  privateChannels: { id: number; name: string ; imgPath: string ; status: boolean }[]=[  //Example Data
     { id: 1, name: 'Dirk Müller', imgPath: 'img/avatars/avatar3.svg', status: true }, 
     { id: 2, name: 'Franziska Stark', imgPath: 'img/avatars/avatar2.svg', status: false },
     { id: 1, name: 'Dirk Müller', imgPath: 'img/avatars/avatar3.svg', status: true }, 
@@ -26,7 +26,6 @@ export class MenuPrivateMessagesComponent {
     { id: 2, name: 'Franziska Stark', imgPath: 'img/avatars/avatar2.svg', status: false },
     { id: 1, name: 'Dirk Müller', imgPath: 'img/avatars/avatar3.svg', status: true }, 
     { id: 2, name: 'Franziska Stark', imgPath: 'img/avatars/avatar2.svg', status: false },
-
   ]
 
 
