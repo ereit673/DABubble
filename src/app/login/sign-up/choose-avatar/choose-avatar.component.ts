207f2f145b13f2fc745ba7cf8a2a1c99ed5b448c
Marcel Zalec <marci.zalec@hotmail.com>
2024-12-22 20:57:12 +0100

    added username to chose avatar component

--- src/app/login/sign-up/choose-avatar/choose-avatar.component.ts
+++ src/app/login/sign-up/choose-avatar/choose-avatar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,12 +10,13 @@ import { Router } from '@angular/router';
   styleUrl: './choose-avatar.component.scss'
 })
 export class ChooseAvatarComponent {
-  name:string = "Frederik Beck";
+  // name:string = "Frederik Beck";
   activePic:number = -1 ;
   profilesPics: string[] = ['avatar2.svg','avatar1.svg','avatar3.svg','avatar6.svg','avatar5.svg','avatar4.svg',];
   path:string = "/img/avatars/";
 
   constructor(private router: Router) {}
+  @Input() name: string = '';
   @Output() backward = new EventEmitter<void>();
   
   back() {
