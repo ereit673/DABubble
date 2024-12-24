d11fc4c236d9aa71e4d4cfbdeecf8a68678b0c26
CybXII <luft.luft86@googlemail.com>
2024-12-24 15:34:49 +0100

    -reworked threadchat html -added Observable array for rendering threadmessages

--- src/app/main/threadchat/threadchat.component.ts
+++ src/app/main/threadchat/threadchat.component.ts
@@ -1,40 +1,32 @@
-import { Component, Input } from '@angular/core';
-import { ChatboxComponent } from '../shared/chatbox/chatbox.component';
+import { Component, Input, OnInit } from '@angular/core';
 import { MessageboxComponent } from '../shared/messagebox/messagebox.component';
 import { ThreadchatHeaderComponent } from './threadchat-header/threadchat-header.component';
-import { Thread } from '../../models/thread';
+import { ThreadMessage } from '../../models/message';
 import { MessagesService } from '../../shared/services/messages.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { ChatboxComponent } from '../shared/chatbox/chatbox.component';
+
 
 @Component({
   selector: 'app-threadchat',
-  standalone: true,   // <-- Add this line
-  imports: [ThreadchatHeaderComponent, MessageboxComponent],
+  standalone: true,
+  imports: [ThreadchatHeaderComponent, ChatboxComponent,MessageboxComponent,CommonModule],
   templateUrl: './threadchat.component.html',
-  styleUrl: './threadchat.component.scss',
+  styleUrls: ['./threadchat.component.scss'],
 })
-export class ThreadchatComponent {
+export class ThreadchatComponent implements OnInit {
   @Input() builder!: string;
   @Input() parentMessage: any = null; // Parent-Message als Input
-  threadMessages: any[] = []; // Thread-Nachrichten
+  threadMessages$!: Observable<ThreadMessage[]>; // Thread-Nachrichten
 
   constructor(private messagesService: MessagesService) {}
 
-  
   ngOnInit(): void {
-    if (!this.parentMessage) {
-      console.warn('parentMessage ist nicht definiert. Es werden Standardwerte verwendet.');
-      this.parentMessage = {
-        avatarPath: '/img/default-avatar.svg',
-        name: 'Unbekannt',
-        description: '',
-        messageDate: '',
-        messageTime: '',
-      };
-    }
-    // Abonniere Thread-Nachrichten aus dem MessagesService
-    this.messagesService.threadMessages$.subscribe((threadMessages) => {
-      this.threadMessages = threadMessages || []; // Leeres Array bei undefined
-      console.log('Thread-Nachrichten aktualisiert:', this.threadMessages);
-    });
+    this.threadMessages$ = this.messagesService.threadMessages$;
+  }
+
+  trackByThreadMessageId(index: number, threadMessage: ThreadMessage): string {
+    return threadMessage.docId!;
   }
 }
