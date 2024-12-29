7b28ec0af32f3af2f8e26cce4839dbfa58e0a23b
Marcel Zalec <marci.zalec@hotmail.com>
2024-12-29 17:04:08 +0100

    added photoURL to userData

--- src/app/login/sign-up/choose-avatar/choose-avatar.component.ts
+++ src/app/login/sign-up/choose-avatar/choose-avatar.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../shared/services/auth.service';
+// import { SignUpComponent } from '../sign-up.component'
 
 @Component({
   selector: 'app-choose-avatar',
-  imports: [CommonModule],
+  imports: [ CommonModule ],
   standalone: true,   // <-- Add this line
   templateUrl: './choose-avatar.component.html',
   styleUrl: './choose-avatar.component.scss'
@@ -15,8 +17,15 @@ export class ChooseAvatarComponent {
   profilesPics: string[] = ['avatar2.svg','avatar1.svg','avatar3.svg','avatar6.svg','avatar5.svg','avatar4.svg',];
   path:string = "/img/avatars/";
 
-  constructor(private router: Router) {}
-  @Input() name: string = '';
+  constructor(private router: Router, private auth: AuthService) {} // private signUp: SignUpComponent
+  // @Input() name: string = '';
+  @Input() userData: any = {
+    name: "",
+    email: "",
+    password: "",
+    privacy: false,
+    photoURL: "",
+  };
   @Output() backward = new EventEmitter<void>();
 
   showSuccess: boolean = false;
@@ -35,5 +44,13 @@ export class ChooseAvatarComponent {
 
   onSignup(){
     this.showSuccess = true;
+  }
+
+  signUpAvatar() {
+    this.userData.photoURL = `${this.path}${this.profilesPics[this.activePic]}`;
+    console.log(this.userData);
+    this.auth.register(this.userData.email, this.userData.password);
+    console.log("mail sent to", this.userData.email);
+    this.onSignup();
   }
 }
