d4b20c38faea594e9d4cb2e5c323468b05522ca1
Lukas Nolting <info@lukas-nolting.eu>
2025-01-06 17:57:36 +0100

    search user, services & more!

--- src/app/shared/header/searchbar/searchbar.component.ts
+++ src/app/shared/header/searchbar/searchbar.component.ts
@@ -12,28 +12,62 @@ import { TimestampToDatePipe } from '../../../pipes/timestamp-to-date.pipe';
   styleUrl: './searchbar.component.scss',
 })
 export class SearchbarComponent {
-  searchResultsActive = false;
+  messageResultsActive = false;
   searchText: string = '';
-  searchResults: any[] = [];
+  messageResults: any[] = [];
+  userResults: any[] = [];
   isSearchActive: boolean = false;
   isSearchTouched: boolean = false;
 
   constructor(private searchService: SearchService) {
-    this.searchService.searchResults$.subscribe((results) => {
-      this.searchResults = results;
-      console.log('Search results:', this.searchResults);
+    this.searchService.messageResults$.subscribe((results) => {
+      this.messageResults = results;
+      console.log('Search results messages:', this.messageResults);
+    });
+    this.searchService.userResults$.subscribe((results) => {
+      this.userResults = results;
+      console.log('Search results user:', this.userResults);
     });
   }
 
   onInputChange(): void {
+    if (this.searchText.length == 1) {
+      this.searchService.loadMessages();
+      this.searchService.loadUsers();
+    }
+
     this.isSearchActive = this.searchText.length >= 4;
+
     this.isSearchTouched = this.searchText.length > 0;
+
     if (this.searchText.length >= 4) {
       this.searchService.searchMessages(this.searchText);
+      this.searchService.searchUsers(this.searchText);
     }
   }
 
-  goToSearchResult(channelId: string, docId: string): void {
-    console.log('Navigating to:', channelId, docId);
-}
+  clearSearch(): void {
+    this.searchText = '';
+    this.isSearchActive = false;
+    this.isSearchTouched = false;
+    this.messageResults = [];
+  }
+
+  goToSearchResult(
+    channelId: string | null,
+    docId: string | null,
+    userId: string | null
+  ): void {
+    console.log(
+      'Navigating to:',
+      'channelId: ',
+      channelId,
+      'docId: ',
+      docId,
+      'userId: ',
+      userId
+    );
+    alert('Zu früh gefreut, ist noch nicht fertig!');
+    this.clearSearch();
+  }
 }
