dc6f42f201977c46065575759d368f142ae5d064
Christoph Völker <christov@gmx.de>
2025-01-06 15:29:22 +0100

    update

--- src/app/main/menu/menu-header/menu-header.component.ts
+++ src/app/main/menu/menu-header/menu-header.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddchatComponent } from '../../addchat/addchat.component';
+import { Channel } from '../../../models/channel';
+import { ChannelsService } from '../../../shared/services/channels.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-menu-header',
@@ -9,7 +12,16 @@ import { AddchatComponent } from '../../addchat/addchat.component';
   templateUrl: './menu-header.component.html',
   styleUrl: './menu-header.component.scss'
 })
-export class MenuHeaderComponent {
+export class MenuHeaderComponent implements OnInit, OnDestroy {
+  private unsubscribeFn: (() => void) | null = null;
+  channelsOpen: boolean = false;
+  channelActive: boolean = false;
+  channels: Channel[] = [];
+  loading: boolean = true;
+  channelForm: FormGroup;
+  currentChannelId: string | null = null;
+channel: any;
+
 
   openNewMessage() {
     this.dialog.open(AddchatComponent, {
@@ -20,7 +32,26 @@ export class MenuHeaderComponent {
   }
 
 
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private fb: FormBuilder,
+    private dialog: MatDialog,
+    public channelsService: ChannelsService,
+  ) {
+    this.channelForm = this.fb.group({
+      name: ['', Validators.required],
+      description: [''],
+      isPrivate: [false],
+    });
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
+  selectChannel(channelId: string): void {
+    this.channelsService.selectChannel(channelId)
+  }
 
 }
