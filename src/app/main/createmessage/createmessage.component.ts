1168e0658eee91e83a1d0a0314b72f4219362ea4
Christoph Völker <christov@gmx.de>
2025-01-10 22:13:23 +0100

    update

--- src/app/main/createmessage/createmessage.component.ts
+++ src/app/main/createmessage/createmessage.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Importiere FormsModule
 import { SearchService } from '../../shared/services/search.service';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-createmessage',
@@ -16,32 +17,47 @@ export class CreatemessageComponent {
   channelResults: any[] = [];
   mailadressResults: any[] = [];
 
-  constructor(private searchService: SearchService) {
+
+  constructor(private searchService: SearchService, private authService: AuthService) {
+    this.searchService.loadUsers(this.userId);
+    this.searchService.loadChannels();
+
     this.searchService.userResults$.subscribe((results) => {
       this.userResults = results;
-      console.log('Search results user:', this.userResults);
     });
+
     this.searchService.channelResults$.subscribe((results) => {
       this.channelResults = results;
-      console.log('Search results channel:', this.channelResults);
     });
+
   }
 
 
   onInputChange() {
-    console.log("du schreibst!");
-    if (this.searchText.length == 1) {
-      this.searchService.loadMessages();
-      this.searchService.loadUsers();
-      this.searchService.loadChannels();
-      this.searchService.searchUsers(this.searchText);
-      //this.searchService.searchChannels(this.searchText);
+    console.log("du schreibst was!");
+    if (this.searchText[0] === "#") {
+      if (this.searchText.length == 1) { //alle!
+        this.searchService.searchChannels('', this.userId, 'channel');
+      }
+      else {
+        this.searchService.searchChannels(this.searchText[1].slice(1), this.userId, 'channel');
+
+      }
     }
-
-
+    else if (this.searchText[0] === "@") {
+      if (this.searchText.length == 1) { // alle!
+        this.searchService.searchUsers('', 'name');
+      }
+      else {
+        //this.searchService.searchUsers(this.searchText[1].slice(1), 'name');
+      }
+    }
   }
 
 
-
+  get userId() {
+    return this.authService.userId() as string;
+  }
 
 }
+
