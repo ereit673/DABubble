b1f82b838257ef3d3cd05abca30acd40bef92c17
Cyb_XII <luft.luft86@googlemail.com>
2025-01-22 01:59:20 +0100

    added resize logik for mobile size

--- src/app/shared/builder/builder.component.ts
+++ src/app/shared/builder/builder.component.ts
@@ -9,61 +9,84 @@ import { MessagesService } from '../services/messages.service';
 
 @Component({
   selector: 'app-builder',
-  standalone: true,   // <-- Add this line
-  imports: [MainchatComponent, ThreadchatComponent,MenuComponent, MenutogglerComponent, CommonModule],
+  standalone: true,
+  imports: [MainchatComponent, ThreadchatComponent, MenuComponent, MenutogglerComponent, CommonModule],
   templateUrl: './builder.component.html',
-  styleUrl: './builder.component.scss',
-  animations: [slideAnimationRight, slideAnimationLeft]
+  styleUrls: ['./builder.component.scss'],
+  animations: [slideAnimationRight, slideAnimationLeft],
 })
 export class BuilderComponent {
-  builder: string[] = ["menu", "mainchat", "threadchat"];
+  builder: string[] = ['menu', 'mainchat', 'threadchat'];
   menuOpened = true;
   threadchatOpened = false;
   menuState = 'in';
   threadchatState = 'out';
   smallWindow = false;
-  
-  
+  mobile = true;
 
   constructor(private messagesService: MessagesService) {}
 
-  
   @HostListener('window:resize', [])
-  onResize(): void {
-    this.smallWindow = window.innerWidth <= 1400;
-  }
+onResize(): void {
+  const wasMobile = this.mobile;
+  this.smallWindow = window.innerWidth <= 1400;
+  this.mobile = window.innerWidth <= 900;
 
+  if (this.mobile && !wasMobile) {
+    // Wenn wir in den Mobile-Modus wechseln, Menü schließen
+    this.menuOpened = false;
+  } else if (!this.mobile && wasMobile) {
+    // Wenn wir den Mobile-Modus verlassen, Menü wieder öffnen
+    this.menuOpened = true;
+  }
+}
 
   ngOnInit(): void {
-    this.onResize(); // Initial prüfen, ob mobil oder Desktop
+    this.onResize();
 
     this.messagesService.threadchatState$.subscribe((state) => {
       this.threadchatState = state ? 'in' : 'out';
     });
   }
 
-
-  toggleMenu() {
-    if (this.menuState === 'in') {
-      this.menuState = 'out';
-      setTimeout(() => {
-        this.menuOpened = false;
-      }, 300);
-    } else {
-      this.menuOpened = true;
-      setTimeout(() => {
-        this.menuState = 'in';
-      });
+  toggleMenu(): void {
+    this.menuState = this.menuState === 'in' ? 'out' : 'in';
+    this.menuOpened = this.menuState === 'in';
+  
+    // Mobile Logik: Schließe Threadchat, wenn Menü geöffnet wird
+    if (this.mobile && this.menuOpened) {
+      this.threadchatOpened = false;
+      this.threadchatState = 'out';
     }
   }
   
+  // toggleThreadChat(): void {
+  //   if (this.threadchatState === 'in') {
+  //     this.threadchatState = 'out';
+  //     setTimeout(() => {
+  //       this.threadchatOpened = false;
+  //     }, 300);
+  //   } else {
+  //     this.threadchatOpened = true;
+  //     setTimeout(() => {
+  //       this.threadchatState = 'in';
+  //     });
+  //   }
+  // }
+
   toggleThreadChat(): void {
+
     if (this.threadchatState === 'in') {
       this.threadchatState = 'out';
       setTimeout(() => {
         this.threadchatOpened = false;
       }, 300);
     } else {
+      if (this.mobile && this.menuOpened) {
+        // Im mobilen Modus Menü schließen, wenn Threadchat geöffnet wird
+        // this.closeMenu();
+      }
+
       this.threadchatOpened = true;
       setTimeout(() => {
         this.threadchatState = 'in';
@@ -71,12 +94,36 @@ export class BuilderComponent {
     }
   }
 
-  toggleThreadChatOnMessageSelect(): void {
-    this.threadchatOpened = true;
-    this.threadchatState = 'in';
+  closeMenu(): void {
+    this.menuState = 'out';
+    setTimeout(() => {
+      this.menuOpened = false;
+    }, 300);
   }
 
-  onAnimationDone(event: any, type: string) {
+  shouldRenderMenu(): boolean {
+    return this.menuOpened || !this.smallWindow;
+  }
+  
+  shouldRenderMainChat(): boolean {
+    if (this.mobile){
+      return !this.menuOpened && !this.threadchatOpened
+    } else if (this.smallWindow && !this.threadchatOpened){
+      return true;
+    }
+    else {
+      return true
+    }
+  }
+  
+  shouldRenderThreadChat(): boolean {
+    if (!this.mobile){
+      return true}
+    else 
+      return this.threadchatOpened;
+  }
+
+  onAnimationDone(event: any, type: string): void {
     if (event.toState === 'in') {
       if (type === 'menu') {
         this.menuOpened = true;
@@ -84,8 +131,7 @@ export class BuilderComponent {
       if (type === 'threadchat') {
         this.threadchatOpened = true;
       }
-    }
-    else if(event.toState === 'out') {
+    } else if (event.toState === 'out') {
       if (type === 'menu') {
         this.menuOpened = false;
       }
@@ -93,9 +139,5 @@ export class BuilderComponent {
         this.threadchatOpened = false;
       }
     }
-  }
-
-  shouldHideMainChat(): boolean {
-    return this.smallWindow && this.threadchatOpened;
   }
 }
