f986d502458d32f20bd2f706bde9e4b81cc30801
CybXII <luft.luft86@googlemail.com>
2025-02-01 01:40:44 +0100

    cleanup added user service functions

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -15,34 +15,50 @@ interface User {
   providedIn: 'root',
 })
 export class UserService {
-  private userCache = new Map<string, User>();
-  private userSubject = new BehaviorSubject<Map<string, User>>(new Map());
+  private userCache = new Map<string, BehaviorSubject<User>>();
 
   constructor(private firestore: Firestore) {}
 
   getUserById(userId: string): Observable<User> {
-    if (this.userCache.has(userId)) {
-      return of(this.userCache.get(userId)!);
-    }
-
-    return from(getDoc(doc(this.firestore, `users/${userId}`))).pipe(
+    if (this.userCache.has(userId)) 
+      return this.userCache.get(userId)!.asObservable();
+    const userSubject = new BehaviorSubject<User>({
+      userId,
+      name: 'Unbekannt',
+      photoURL: '',
+      email: '',
+      status: false,
+    });
+    this.userCache.set(userId, userSubject);
+    from(getDoc(doc(this.firestore, `users/${userId}`))).pipe(
       map((docSnap) => {
         if (docSnap.exists()) {
           const userData = { userId, ...docSnap.data() } as User;
-          this.userCache.set(userId, userData);
-          this.userSubject.next(this.userCache);
-          return userData;
+          userSubject.next(userData); 
         }
-        return { userId, name: 'Unbekannt', photoURL: '', email: '', status: false };
       })
-    );
+    ).subscribe();
+    return userSubject.asObservable();
   }
 
-  getuserAvatar(userId: string) {
+
+  getuserAvatar(userId: string): Observable<string> {
     return this.getUserById(userId).pipe(
       map((user) => user.photoURL || '/img/avatars/picPlaceholder.svg')
     );
   }
 
-  
+
+  getuserStatus(userId: string): Observable<string> {
+    return this.getUserById(userId).pipe(
+      map((user) => user.status ? 'online' : 'offline')
+    );
+  }
+
+
+  getuserName(userId: string): Observable<string> {
+    return this.getUserById(userId).pipe(
+      map((user) => user.name ? user.name : 'Unbekannt')
+    );
+  }
 }
