e1803b6900d80ae2d4e5dca34fa30eb9a1e67e70
CybXII <luft.luft86@googlemail.com>
2025-02-16 00:02:41 +0100

    cleanup threadchat-header

--- src/app/main/chat/threadchat-header/threadchat-header.component.ts
+++ src/app/main/chat/threadchat-header/threadchat-header.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MessagesService } from '../../../shared/services/messages.service';
 import { ChannelsService } from '../../../shared/services/channels.service';
 import { Channel } from '../../../models/channel';
 import { Observable } from 'rxjs';
@@ -7,30 +6,44 @@ import { StateService } from '../../../shared/services/state.service';
 
 @Component({
   selector: 'app-threadchat-header',
-  standalone: true,   // <-- Add this line
+  standalone: true,
   imports: [],
   templateUrl: './threadchat-header.component.html',
   styleUrl: './threadchat-header.component.scss'
 })
 export class ThreadchatHeaderComponent {
-    currentChannel$: Observable<Channel | null>;
-    channelName: string = '';
+  currentChannel$: Observable<Channel | null>;
+  channelName: string = '';
 
-    constructor(private stateService: StateService, private channelsService: ChannelsService) {
-      this.currentChannel$ = this.channelsService.currentChannel$;
-    }
-
-
-    ngOnInit(): void {
-      this.currentChannel$.subscribe((channel) => {
-        if (channel) {
-          this.channelName = channel.name;
-        }
-      });
-    }
+/**
+ * Constructs a new instance of the ThreadchatHeaderComponent.
+ *
+ * @param stateService The service used to manage the state of the thread chat.
+ * @param channelsService The service providing the current channel data.
+ */
+  constructor(private stateService: StateService, private channelsService: ChannelsService) {
+    this.currentChannel$ = this.channelsService.currentChannel$;
+  }
 
 
-    closeThreadChat(): void {
-      this.stateService.setThreadchatState('out');
-    }
+/**
+ * OnInit lifecycle hook. Subscribes to the current channel observable
+ * and updates the channel name on each emission.
+ */
+  ngOnInit(): void {
+    this.currentChannel$.subscribe((channel) => {
+      if (channel) {
+        this.channelName = channel.name;
+      }
+    });
+  }
+
+
+  /**
+   * Closes the thread chat by setting the thread chat state to 'out'.
+   * This causes the thread chat to be hidden and the main chat to be shown.
+   */
+  closeThreadChat(): void {
+    this.stateService.setThreadchatState('out');
+  }
 }
