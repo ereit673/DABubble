b43e67d178fba1d7448dac685ad6589545e9c60b
CybXII <luft.luft86@googlemail.com>
2025-02-16 20:27:56 +0100

    cleanup state service

--- src/app/shared/services/state.service.ts
+++ src/app/shared/services/state.service.ts
@@ -7,18 +7,32 @@ import { BehaviorSubject } from 'rxjs';
 export class StateService {
   private menuStateSubject = new BehaviorSubject<string>('in');
   private threadchatStateSubject = new BehaviorSubject<string>('out');
-
   menuState$ = this.menuStateSubject.asObservable();
   threadchatState$ = this.threadchatStateSubject.asObservable();
 
+
+  /**
+   * Sets the menu state to the given value and emits it to all subscribers of the menuState$ observable.
+   * @param state - The new menu state, either 'in' or 'out'.
+   */
   setMenuState(state: string): void {
     this.menuStateSubject.next(state);
   }
 
+
+  /**
+   * Sets the thread chat state to the given value and emits it to all subscribers of the threadchatState$ observable.
+   * @param state - The new thread chat state, either 'in' or 'out'.
+   */
   setThreadchatState(state: string): void {
     this.threadchatStateSubject.next(state);
   }
 
+
+  /**
+   * Sets both the menu and thread chat state to 'out' and emits it to all subscribers of the menuState$ and threadchatState$ observables.
+   * This is a convenience method to close both the menu and the thread chat in one method call.
+   */
   closeMenuAndThread(): void {
     this.menuStateSubject.next('out');
     this.threadchatStateSubject.next('out');
