e3483b5a30cfd272dedb6d78179fbceb28871233
CybXII <luft.luft86@googlemail.com>
2025-02-16 19:51:50 +0100

    cleanup emoji-storage service

--- src/app/shared/services/emoji-storage.service.ts
+++ src/app/shared/services/emoji-storage.service.ts
@@ -6,24 +6,31 @@ import { Injectable } from '@angular/core';
 export class EmojiStorageService {
   private storageKey = 'recentEmojis';
 
-  constructor() {}
-
+  /**
+   * Saves an emoji to the local storage so it can be suggested later.
+   *
+   * It keeps track of the two most recently used emojis. If the emoji is
+   * already in the list, it does nothing. Otherwise it adds it to the list
+   * and removes the oldest one if there are more than two emojis in the list.
+   * @param emoji The emoji to save.
+   */
   saveEmoji(emoji: string): void {
     let emojis: string[] = JSON.parse(
       localStorage.getItem(this.storageKey) || '[]'
     );
-
     if (emojis[1] === emoji) return;
-
     emojis = emojis.filter((e) => e !== emoji);
-
     emojis.unshift(emoji);
-
     emojis = emojis.slice(0, 2);
-
     localStorage.setItem(this.storageKey, JSON.stringify(emojis));
   }
 
+  /**
+   * Retrieves the list of two most recently used emojis from the local storage.
+   *
+   * @returns An array of two strings, each representing an emoji. The first
+   *     element is the most recently used emoji.
+   */
   getEmojis(): string[] {
     return JSON.parse(localStorage.getItem(this.storageKey) || '[]');
   }
