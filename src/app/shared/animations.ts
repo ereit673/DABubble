23edecc5a2c08fb36996ae2f7a67684d2136366c
CybXII <luft.luft86@googlemail.com>
2025-02-21 14:51:49 +0100

    adjusted animation

--- src/app/shared/animations.ts
+++ src/app/shared/animations.ts
@@ -2,18 +2,16 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 
 
 export const slideAnimationLeft = trigger('slideAnimationLeft', [
-  state('in', style({width: '{{width}}','min-width': '{{width}}',transform: 'translateX(0)',overflow: 'hidden'}), { params: { width: '366px' }}),
-  state('out', style({width: '0',transform: 'translateX(-100%)',overflow: 'hidden'})),
+  state('in', style({width: '{{width}}', transform: 'translateX(0)', overflow: 'hidden' }), { params: { width: '100%' }, }),
+  state('out', style({ width: '0', transform: 'translateX(-100%)', overflow: 'hidden' })),
   transition('in => out', [
-    animate('300ms ease-in-out', 
-      style({ transform: 'translateX(-100%)', width: '0' }))
+    animate('300ms ease-in-out', style({ transform: 'translateX(-300px)', width: '0px' })), // Zwischenschritt
+    animate('300ms ease-in-out')
   ]),
   transition('out => in', [
-    animate('300ms ease-in-out', 
-      style({ width: '0', transform: 'translateX(-100%)' })),
-    animate('300ms ease-in-out', 
-      style({ width: '{{width}}', transform: 'translateX(0)' }))
-  ])
+    animate('300ms ease-in-out', style({ width: '0', transform: 'translateX(-100%)' })), // Breite wächst
+    animate('300ms ease-in-out', style({ width: '{{width}}', transform: 'translateX(0)' })) // Vollständig reinsliden
+  ]),
 ]);
 
 
